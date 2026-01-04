a44c040965c517414d2a34f3a78db40f19d66a59
Mohamed Bahaa9020 <mo.bahaa3020@gmail.com>
2026-01-04 10:52:08 +0200

    FinalEditAndDeleteProfile

---
+++ TimeOfEnter/Migrations/20260104085050_Delete_User.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TimeOfEnter.Migrations
+{
+    /// <inheritdoc />
+    public partial class Delete_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "UserBooking",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "UserBooking");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "AspNetUsers");
+        }
+    }
+}
